0d8d8a6971a4c328914b57eadd488d1047934bdc
Brett Hageft <brett.hageft@gmail.com>
2021-12-09 11:34:46 -0700

    landing style finished

--- frontend/src/components/Landing/index.js
+++ frontend/src/components/Landing/index.js
@@ -1,12 +1,18 @@
-
+import { signup } from '../../store/session'
+import './Landing.css'
 
 const landing = () => {
     return (
-        <div>
-            <h3>Welcome to GrimWarrior</h3>
-            <p>Well met traveller! Hang up your cloak and pull up a seat around the fire.</p>
-            <p>GrimWarrior is the premier tool for Wizards of all walks to manage their grimoires, tomes, and spellbooks. </p>
-            <p> Sign up today to see why Elminster of Shadowdale no longer returns our calls!</p>
+        <div className="landing-container">
+            <div className='welcome'>
+
+                <h2>Welcome to <span className='intro'>GrimWarrior</span></h2>
+
+                <p>The premier tool chosen by Wizards the Realm over for Grimoire Managment™</p>
+                <button className='edit-delete'>Login</button>
+                <button className='edit-delete'>Sign Up</button>
+            </div>
+
         </div>
     )
 }
